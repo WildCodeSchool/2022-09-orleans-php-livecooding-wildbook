c7495c9d495e0d7846c34a20eef7ac72ab51307c
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.fr>
2023-01-03 15:00:47 +0100

    display image

--- src/DataFixtures/DocumentFixtures.php
+++ src/DataFixtures/DocumentFixtures.php
@@ -7,20 +7,30 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Filesystem\Filesystem;
 
 class DocumentFixtures extends Fixture implements DependentFixtureInterface
 {
-    public const DOCUMENT_NUMBER = 50;
+    public const DOCUMENT_NUMBER = 20;
+
+    public function __construct(private Filesystem $filesystem)
+    {
+    }
 
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
+        $this->filesystem->remove(__DIR__ . '/../../public/uploads/documents/');
+        $this->filesystem->mkdir(__DIR__ . '/../../public/uploads/documents/');
 
         for ($i = 0; $i < self::DOCUMENT_NUMBER; $i++) {
             $document = new Document();
             $document->setTitle($faker->sentence(3));
             $document->setDescription($faker->paragraphs(3, true));
             $document->setIsbn($faker->isbn13());
+            $imageName = uniqid() . '.png';
+            copy('https://loremflickr.com/320/240/book', __DIR__ . '/../../public/uploads/documents/' . $imageName);
+            $document->setImageName($imageName);
             $category = $this->getReference('category' . rand(0, count(CategoryFixtures::CATEGORIES) - 1));
             $document->setCategory($category);
 
